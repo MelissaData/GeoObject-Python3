95d7df39e1576d7d323a0361bd2487ec680909f0
Melissa <>
2025-07-23 09:11:50 -0700

    Tested successfully using Melissa data files for 2025.Q3

--- MelissaGeoCoderObjectWindowsPython3.ps1
+++ MelissaGeoCoderObjectWindowsPython3.ps1
@@ -19,7 +19,7 @@ class FileConfig {
 
 ######################### Config ###########################
 
-$RELEASE_VERSION = '2025.Q2'
+$RELEASE_VERSION = '2025.Q3'
 $ProductName = "GEOCODER_DATA"
 
 # Uses the location of the .ps1 file 
